f4a482712d9993b7fcceba23aebde5f57ed1a49b
刘旭 <xuliu@yunrang.com>
2013-07-19 12:11:33 +0800

    Init project with gradle

---
+++ src/main/java/com/xu/jlib/A.java
@@ -0,0 +1,5 @@
+package com.xu.jlib;
+
+public class A{
+  public static final String str = "Hello World!";
+}
---
+++ src/main/java/com/xu/jlib/Main.java
@@ -0,0 +1,11 @@
+package com.xu.jlib;
+
+public class Main {
+
+  private String str = A.str;
+  
+  public static void main(String[] args){
+
+    System.out.println(new Main().str);
+  }
+}
